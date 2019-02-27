1c2a274470b5885cbe8a36eda3616fa01560eb6f
Brandon121 <47826720+Brandon121@users.noreply.github.com>
2019-02-27 10:57:49 -0500

    Create while.c

---
+++ while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int a=10;
+    int b=11;
+    while(a<20) {
+        printf("value of a is %d\n\n\n", a);
+        a = a+1;
+        
+    }
+    while (b>0){
+        
+        printf("value of b is %d\n\n\n",b);
+        b=b-1;
+        
+    }
+    return 0;
+}
